5a3e774c899c5b177a6bfb615889f282e0ddf8a4
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2013-10-22 17:27:41 +0200

    ENH: clean useless comments

--- src/scripts/scatterplots.py
+++ src/scripts/scatterplots.py
@@ -50,14 +50,14 @@ nbVariables = len(variables)
 for row in range(nbVariables):
     for col in range(nbVariables):
         ax = plt.subplot(nbVariables, nbVariables, row*nbVariables+col+1)
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
         x = samples[variables[col]]
         ax.set_xlim(bounds[variables[col]]['min'], bounds[variables[col]]['max'])
         if col == 0:
             ax.set_ylabel(variables[row])
+            ax.set_yticklabels([bounds[variables[row]]['min'], bounds[variables[row]]['max']])
         if row == nbVariables-1:
-            ax.set_xlabel(variables[col])            
+            ax.set_xlabel(variables[col])
+            ax.set_xticklabels([bounds[variables[col]]['min'], bounds[variables[col]]['max']])
         if row != col :
             y = samples[variables[row]]
             ax.set_ylim(bounds[variables[row]]['min'], bounds[variables[row]]['max'])
@@ -66,44 +66,3 @@ for row in range(nbVariables):
             ax.hist(x, bins=20)
         
 plt.show()
-# nullfmt   = NullFormatter()         # no labels
-
-# # # definitions for the axes
-# left, width = 0.1, 0.65
-# bottom, height = 0.1, 0.65
-# bottom_h = left_h = left+width+0.02
-
-# rect_scatter = [left, bottom, width, height]
-# rect_histx = [left, bottom_h, width, 0.2]
-# rect_histy = [left_h, bottom, 0.2, height]
-
-# # start with a rectangular Figure
-# plt.figure(1, figsize=(8,8))
-
-# axScatter = plt.axes(rect_scatter)
-# axHistx = plt.axes(rect_histx)
-# axHisty = plt.axes(rect_histy)
-
-# # no labels
-# axHistx.xaxis.set_major_formatter(nullfmt)
-# axHisty.yaxis.set_major_formatter(nullfmt)
-
-# # the scatter plot:
-# axScatter.scatter(x, y)
-
-# # now determine nice limits by hand:
-# binwidth = 0.25
-# xymax = np.max( [np.max(np.fabs(x)), np.max(np.fabs(y))] )
-# lim = ( int(xymax/binwidth) + 1) * binwidth
-
-# axScatter.set_xlim( (-lim, lim) )
-# axScatter.set_ylim( (-lim, lim) )
-
-# bins = np.arange(-lim, lim + binwidth, binwidth)
-# axHistx.hist(x, bins=bins)
-# axHisty.hist(y, bins=bins, orientation='horizontal')
-
-# axHistx.set_xlim( axScatter.get_xlim() )
-# axHisty.set_ylim( axScatter.get_ylim() )
-
-# plt.show()
