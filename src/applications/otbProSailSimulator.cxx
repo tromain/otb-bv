8958e8c1f47c79364218b13f7a0a2dd315b5685b
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2013-10-23 12:26:54 +0200

    ENH: Integration of the simulation functor. Stub.

--- src/applications/otbProSailSimulator.cxx
+++ src/applications/otbProSailSimulator.cxx
@@ -16,10 +16,118 @@
 #include "otbWrapperApplicationFactory.h"
 #include "otbWrapperChoiceParameter.h"
 
+#include "otbLeafParameters.h"
+#include "otbSailModel.h"
+#include "otbProspectModel.h"
+#include "otbSatelliteRSR.h"
+#include "otbReduceSpectralResponse.h"
+
 #include <fstream>
 
 namespace otb
 {
+
+namespace Functor
+{
+
+template <class TSatRSR, unsigned int SimNbBands = 2000>
+class ProsailSimulatorFunctor
+{
+public:
+  /** Standard class typedefs */
+  typedef TSatRSR SatRSRType;
+  typedef typename SatRSRType::Pointer SatRSRPointerType;
+  typedef typename otb::ProspectModel ProspectType;
+  typedef typename otb::SailModel SailType;
+
+  typedef double PrecisionType;
+  typedef std::pair<PrecisionType,PrecisionType> PairType;
+  typedef typename std::vector<PairType> VectorPairType;
+  typedef otb::SpectralResponse< PrecisionType, PrecisionType>  ResponseType;
+  typedef ResponseType::Pointer  ResponsePointerType;
+  typedef otb::ReduceSpectralResponse < ResponseType,SatRSRType>  ReduceResponseType;
+  typedef typename ReduceResponseType::Pointer  ReduceResponseTypePointerType;
+  typedef typename std::vector<PrecisionType> OutputType;
+  
+  /** Constructor */
+  ProsailSimulatorFunctor() {
+    m_SatRSR = SatRSRType::New();
+  }
+
+  /** Destructor */
+  ~ProsailSimulatorFunctor() {};
+  
+  /** Implementation of the () operator*/
+  inline OutputType operator ()()
+  {
+    OutputType pix;
+    for(unsigned int i=0;i<m_SatRSR->GetNbBands();i++)
+      pix.push_back(0.0);
+    VectorPairType hxSpectrum;
+    for(unsigned int i=0;i<SimNbBands;i++)
+      {
+      PairType resp;
+      resp.first = static_cast<PrecisionType>((400.0+i)/1000);
+      hxSpectrum.push_back(resp);
+      }
+    ProspectType::Pointer prospect = ProspectType::New();
+    //  prospect->SetInput(m_LabelParameters[ label ]);
+    SailType::Pointer sail = SailType::New();
+/*    sail->SetLAI(lai);
+    sail->SetAngl(m_AcquisitionParameters[std::string("Angl")]);
+    sail->SetPSoil(m_AcquisitionParameters[std::string("PSoil")]);
+    sail->SetSkyl(m_AcquisitionParameters[std::string("Skyl")]);
+    sail->SetHSpot(m_AcquisitionParameters[std::string("HSpot")]);
+    sail->SetTTS(m_AcquisitionParameters[std::string("TTS")]);
+    sail->SetTTO(m_AcquisitionParameters[std::string("TTO")]);
+    sail->SetPSI(m_AcquisitionParameters[std::string("PSI")]);
+    sail->SetReflectance(prospect->GetReflectance());
+    sail->SetTransmittance(prospect->GetTransmittance());*/
+    sail->Update();
+    for(unsigned int i=0;i<SimNbBands;i++)
+      {
+      hxSpectrum[i].second = static_cast<PrecisionType>(sail->GetHemisphericalReflectance()->GetResponse()[i].second);
+      }
+    ResponseType::Pointer aResponse = ResponseType::New();
+    aResponse->SetResponse( hxSpectrum );
+    ReduceResponseTypePointerType  reduceResponse = ReduceResponseType::New();
+    reduceResponse->SetInputSatRSR(m_SatRSR);
+    reduceResponse->SetInputSpectralResponse( aResponse );
+    reduceResponse->CalculateResponse();
+    VectorPairType reducedResponse =  reduceResponse->GetReduceResponse()->GetResponse();
+    for(unsigned int i=0;i<m_SatRSR->GetNbBands();i++)
+      pix[i] = reducedResponse[i].second;
+    return pix;
+  }
+
+  bool operator !=(const ProsailSimulatorFunctor& other) const
+  {
+    return true;
+  }
+
+  bool operator ==(const ProsailSimulatorFunctor& other) const
+  {
+    return false;
+  }
+
+  inline void SetRSR(const  SatRSRPointerType rsr)
+  {
+    m_SatRSR = rsr;
+  }
+
+  inline SatRSRPointerType GetRSR() const
+  {
+    return m_SatRSR;
+  }
+  
+protected:
+
+  /** Satellite Relative spectral response*/
+  SatRSRPointerType m_SatRSR;
+};
+
+
+}
 
 namespace Wrapper
 {
@@ -57,9 +165,9 @@ private:
     SetParameterDescription( "in", "Input file containing ." );
     MandatoryOn("soil-file");
 
-    AddParameter(ParameterType_InputFilename, "srs-file", "Input file containing the spectral relative sensitivities..");
+    AddParameter(ParameterType_InputFilename, "rsr-file", "Input file containing the relative spectral responses.");
     SetParameterDescription( "in", "Input file containing ." );
-    MandatoryOn("srs-file");
+    MandatoryOn("rsr-file");
     
     AddParameter(ParameterType_OutputFilename, "out", "Output file");
     SetParameterDescription( "out", "Filename where the simulations are saved." );
@@ -92,14 +200,18 @@ private:
 
   void DoExecute()
   {
-
+    m_Azimuth = GetParameterFloat("azimuth");
+    m_SolarZenith = GetParameterFloat("solar-zenith");
+    m_SensorZenith = GetParameterFloat("sensor-zenith");
     
 
   }
 
+  double m_Azimuth;
+  double m_SolarZenith;
+  double m_SensorZenith;
   // the input file
   std::ifstream m_SampleFile;
-
   // the output file
   std::ofstream m_SimulationsFile;
 };
