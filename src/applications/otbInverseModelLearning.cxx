ecb166fc6ffa6b389841bbf03efd86464b3931ba
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2013-10-24 09:43:22 +0200

    ADD: stub for the inverse model learning

---
+++ src/applications/otbInverseModelLearning.cxx
@@ -0,0 +1,108 @@
+/*=========================================================================
+  Program:   otb-bv
+  Language:  C++
+
+  Copyright (c) CESBIO. All rights reserved.
+
+  See otb-bv-copyright.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "otbWrapperApplication.h"
+#include "otbWrapperApplicationFactory.h"
+#include "otbWrapperChoiceParameter.h"
+
+#include <fstream>
+
+namespace otb
+{
+
+namespace Wrapper
+{
+
+class InverseModelLearning : public Application
+{
+public:
+/** Standard class typedefs. */
+  typedef InverseModelLearning     Self;
+  typedef Application                   Superclass;
+  typedef itk::SmartPointer<Self>       Pointer;
+  typedef itk::SmartPointer<const Self> ConstPointer;
+
+  
+/** Standard macro */
+  itkNewMacro(Self);
+
+  itkTypeMacro(InverseModelLearning, otb::Application);
+
+  typedef double PrecisionType;
+  
+private:
+  void DoInit()
+  {
+    SetName("InverseModelLearning");
+    SetDescription("Simulate reflectances using Prospect+Sail.");
+
+    AddParameter(ParameterType_InputFilename, "training", "Input file containing the training samples.");
+    SetParameterDescription( "training", "Input file containing the training samples. This is an ASCII file where each line is a training sample. A line is a set of fields containing numerical values. The first field is the value of the output variable and the other contain the values of the input variables." );
+    MandatoryOn("training");
+
+    AddParameter(ParameterType_OutputFilename, "out", "Output regression model.");
+    SetParameterDescription( "out", "Filename where the regression model will be saved." );
+    MandatoryOn("out");
+  }
+
+  virtual ~InverseModelLearning()
+  {
+  }
+
+
+  void DoUpdateParameters()
+  {
+    // Nothing to do here : all parameters are independent
+  }
+
+  
+  void DoExecute()
+  {
+   
+    std::string trainingFileName = GetParameterString("training");
+    std::ifstream trainingFile;
+    try
+      {
+      trainingFile.open(trainingFileName.c_str());
+      }
+    catch(...)
+      {
+      itkGenericExceptionMacro(<< "Could not open file " << trainingFileName);
+      }
+
+    unsigned short int nbInputVariables = countColumns(trainingFile) - 1;
+    while(m_SampleFile.good())
+      {
+      // Read the variable values
+      std::string line;
+      std::getline(m_SampleFile, line);
+      std::stringstream ss(line);
+      double outputValue;
+      ss >> outputValue;
+      for(unsigned int var = 0; var < nbInputVariables; ++ var)
+        {
+        double inputValue;
+        ss >> inputValue;
+        }
+      }
+
+    trainingFile.close();
+  }
+
+};
+
+}
+}
+
+OTB_APPLICATION_EXPORT(otb::Wrapper::InverseModelLearning)
