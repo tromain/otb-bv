32f5fa08453f0d1ccb870306382568f00e47267b
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2013-10-24 09:37:43 +0200

    ENH: avoid multiple definitions

--- src/applications/otbBVTypes.h
+++ src/applications/otbBVTypes.h
@@ -11,7 +11,8 @@
   PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-
+#ifndef __otbBVTypes_h
+#define __otbBVTypes_h
 namespace otb
 {
 enum IVNames {MLAI, ALA, CrownCover, HsD, N, Cab, Cdm, CwRel, Cbp, Bs, IVNamesEnd};
@@ -20,3 +21,4 @@ enum AcquisitionParameters {HSPOT, TTS, TTO, PSI, AcquisitionParametersEnd};
 typedef std::map< AcquisitionParameters, double > AcquisitionParsType;
 typedef std::map< IVNames, double > BVType;
 }
+#endif
