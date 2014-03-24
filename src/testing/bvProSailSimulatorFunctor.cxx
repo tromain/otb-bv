fa5380cfe1259a3e15eeca08752a57ba8196fceb
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2014-03-24 15:27:39 +0100

    TEST: create testing infrastructure

---
+++ src/testing/bvProSailSimulatorFunctor.cxx
@@ -0,0 +1,23 @@
+/*=========================================================================
+  Program:   otb-bv
+  Language:  C++
+
+  Copyright (c) CESBIO. All rights reserved.
+
+  See otb-bv-copyright.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkMacro.h"
+#include "otbProSailSimulatorFunctor.h"
+
+int bvProSailSimulatorFunctor(int argc, char * argv[])
+{
+
+  return EXIT_SUCCESS;
+}
+
---
+++ src/testing/otbBVTests.cxx
@@ -0,0 +1,19 @@
+/*=========================================================================
+  Program:   otb-bv
+  Language:  C++
+
+  Copyright (c) CESBIO. All rights reserved.
+
+  See otb-bv-copyright.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "otbTestMain.h"
+
+void RegisterTests()
+{
+  REGISTER_TEST(bvProSailSimulatorFunctor);
+}
