e8c045435b6e350311ec20b5352df64dcab5e00f
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2014-03-24 14:59:45 +0100

    ENH: remove useless includes

--- src/library/otbBVUtil.h
+++ src/library/otbBVUtil.h
@@ -14,35 +14,11 @@
 #ifndef __otbBVUtil_h
 #define __otbBVUtil_h
 
-#include <fstream>
 #include <string>
-#include "itkMacro.h"
 
 namespace otb
 {
-  unsigned short int countColumns(std::string fileName)
-  {
-    std::ifstream ifile(fileName.c_str());
-    std::string line;
-    auto nbSpaces = 0;
-    if (ifile.is_open())
-      {
-      getline(ifile,line);
-      ifile.close();
-      auto found = line.find(' ');
-      while(found!=std::string::npos)
-        {
-        ++nbSpaces;
-        found = line.find(' ', found+1);
-        }
-      return nbSpaces+1;
-      }
-    else
-      {
-      itkGenericExceptionMacro(<< "Could not open file " << fileName);
-      }
-
-  }
+unsigned short int countColumns(std::string fileName);
 }
 
 #endif
