5d8bd6936fbdb37aa3b10531cf0750e28fbcaaf2
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2016-05-24 09:23:41 +0200

    REFAC: move implementation out of declaration
    in order to allow easier testing

--- src/applications/otbBVInputVariableGeneration.cxx
+++ src/applications/otbBVInputVariableGeneration.cxx
@@ -12,50 +12,90 @@
 
 =========================================================================*/
 
-#include "otbWrapperApplication.h"
-#include "otbWrapperApplicationFactory.h"
-#include "otbWrapperChoiceParameter.h"
-
+#include "otbBVInputVariableGeneration.h"
 #include <random>
 #include <fstream>
 
-#include "otbBVTypes.h"
 
 namespace otb
 {
 
+namespace BV
+{
+template<typename RNGType>
+double Rng(VarParams vpars, RNGType rngen)
+{
+  //TODO : why us stdev defined for uniform?
+  double min = vpars.min;
+  double max = vpars.max;
+  double mod = vpars.mod;
+  double stdev = vpars.std;
+  DistType dist = vpars.dist;
+
+  double rn;
+  if(dist == DistType::GAUSSIAN)
+    {
+    auto sampleInsideBounds = false;
+    while(!sampleInsideBounds)
+      {
+      std::normal_distribution<> d(mod,stdev);
+      rn = d(rngen);
+      if( rn >= min && rn <= max)
+        sampleInsideBounds = true;
+      }
+    }
+  if(dist == DistType::LOGNORMAL)
+    {
+    auto sampleInsideBounds = false;
+    while(!sampleInsideBounds)
+      {
+      std::lognormal_distribution<> d(mod, stdev);
+      rn = d(rngen);
+      if( rn >= min && rn <= max)
+        sampleInsideBounds = true;
+      }
+    }
+  else
+    {
+    std::uniform_real_distribution<> d(min, max);
+    rn = d(rngen);
+    }
+
+
+
+
+  return rn;
+}
+
+/**
+
+        V* = (V-Vmin(0))*(Vmax(LAI)-Vmin(LAI))/(Vmax(0)-Vmin(0))+Vmin(LAI)
+        p 32 of ATBD_BioVar_Venµs_V1.0.pdf
+
+  */
+double CorrelateValue(double v, double lai, VarParams vpars)
+{
+  double Vmin0 = vpars.min;
+  double Vmax0 = vpars.max;
+  double VminLAImax = vpars.Min_LAI_Max;
+  double VmaxLAImax = vpars.Max_LAI_Max;
+  bool codist = vpars.CoDistrib;
+  if(codist)
+    {
+    double VminLAI = Vmin0+lai*(VminLAImax-Vmin0);
+    double VmaxLAI = Vmax0+lai*(VmaxLAImax-Vmax0);
+    double res = (v-Vmin0)/(Vmax0-Vmin0)*(VmaxLAI-VminLAI)+VminLAI;
+    return res<0?0:res;
+    }
+  else
+    return v;
+}
+
+}//namespace BV 
 namespace Wrapper
 {
 
-class BVInputVariableGeneration : public Application
-{
-public:
-/** Standard class typedefs. */
-  typedef BVInputVariableGeneration     Self;
-  typedef Application                   Superclass;
-
-  enum class DistType {GAUSSIAN, UNIFORM, LOGNORMAL};
-
-  struct VarParams {
-    double min;
-    double max;
-    double mod;
-    double std;
-    double Min_LAI_Max;
-    double Max_LAI_Max;
-    bool CoDistrib;
-    unsigned short  nbcl;
-    DistType dist;
-  };
-  
-  /** Standard macro */
-  itkNewMacro(Self);
-
-  itkTypeMacro(BVInputVariableGeneration, otb::Application);
-
-  typedef std::map< IVNames, double > SampleType;
-private:
-  void DoInit()
+  void BVInputVariableGeneration::DoInit()
   {
     SetName("BVInputVariableGeneration");
     SetDescription("Generate random input variable distribution for ... .");
@@ -108,128 +148,50 @@ private:
 
   }
 
-  virtual ~BVInputVariableGeneration()
-  {
-  }
-
-
-  void DoUpdateParameters()
-  {
-    // Nothing to do here : all parameters are independent
-  }
-
-  //Generates a random number of the appropriate distribution and respecting the bounds
-  double Rng(VarParams vpars)
-  {
-    //TODO : why us stdev defined for uniform?
-    double min = vpars.min;
-    double max = vpars.max;
-    double mod = vpars.mod;
-    double stdev = vpars.std;
-    DistType dist = vpars.dist;
-
-    double rn;
-    if(dist == DistType::GAUSSIAN)
-      {
-      auto sampleInsideBounds = false;
-      while(!sampleInsideBounds)
-        {
-        std::normal_distribution<> d(mod,stdev);
-        rn = d(m_RNG);
-        if( rn >= min && rn <= max)
-          sampleInsideBounds = true;
-        }
-      }
-    if(dist == DistType::LOGNORMAL)
-      {
-      auto sampleInsideBounds = false;
-      while(!sampleInsideBounds)
-        {
-        std::lognormal_distribution<> d(mod, stdev);
-        rn = d(m_RNG);
-        if( rn >= min && rn <= max)
-          sampleInsideBounds = true;
-        }
-      }
-    else
-      {
-      std::uniform_real_distribution<> d(min, max);
-      rn = d(m_RNG);
-      }
-
-
-
-
-    return rn;
-  }
-
-
-  /**
-
-        V* = (V-Vmin(0))*(Vmax(LAI)-Vmin(LAI))/(Vmax(0)-Vmin(0))+Vmin(LAI)
-        p 32 of ATBD_BioVar_Venµs_V1.0.pdf
-
-  */
-  double CorrelateValue(double v, double lai, VarParams vpars)
-  {
-    double Vmin0 = vpars.min;
-    double Vmax0 = vpars.max;
-    double VminLAImax = vpars.Min_LAI_Max;
-    double VmaxLAImax = vpars.Max_LAI_Max;
-    bool codist = vpars.CoDistrib;
-    if(codist)
-      {
-      double VminLAI = Vmin0+lai*(VminLAImax-Vmin0);
-      double VmaxLAI = Vmax0+lai*(VmaxLAImax-Vmax0);
-      double res = (v-Vmin0)/(Vmax0-Vmin0)*(VmaxLAI-VminLAI)+VminLAI;
-      return res<0?0:res;
-      }
-    else
-      return v;
-  }
   
-  ///Builds the map with the values of the sample
-  SampleType DrawSample()
-  {
-    SampleType s;
-    s[IVNames::MLAI] = this->Rng(m_MLAI);
-    s[IVNames::ALA] = this->CorrelateValue(this->Rng(m_ALA), 
-                                           s[IVNames::MLAI], m_ALA);
-    s[IVNames::CrownCover] = this->CorrelateValue(this->Rng(m_CrownCover), 
-                                                  s[IVNames::MLAI], m_CrownCover);
-    s[IVNames::HsD] = this->CorrelateValue(this->Rng(m_HsD), 
-                                           s[IVNames::MLAI], m_HsD);
-    s[IVNames::N] = this->CorrelateValue(this->Rng(m_N), 
-                                         s[IVNames::MLAI], m_N);
-    s[IVNames::Cab] = this->CorrelateValue(this->Rng(m_Cab), 
-                                           s[IVNames::MLAI], m_Cab);
-    s[IVNames::Car] = s[IVNames::Cab]*0.25;
-    s[IVNames::Cdm] = this->CorrelateValue(this->Rng(m_Cdm), 
-                                           s[IVNames::MLAI], m_Cdm);
-    s[IVNames::CwRel] = this->CorrelateValue(this->Rng(m_CwRel), 
-                                             s[IVNames::MLAI], m_CwRel);
-    s[IVNames::Cbp] = this->CorrelateValue(this->Rng(m_Cbp), 
-                                           s[IVNames::MLAI], m_Cbp);
-    s[IVNames::Bs] = this->CorrelateValue(this->Rng(m_Bs), 
-                                          s[IVNames::MLAI], m_Bs);
+///Builds the map with the values of the sample
+otb::BV::SampleType BVInputVariableGeneration::DrawSample()
+{
+  using namespace otb::BV;
+  SampleType s;
+  s[IVNames::MLAI] = Rng(m_MLAI, m_RNG);
+  s[IVNames::ALA] = CorrelateValue(Rng(m_ALA, m_RNG), 
+                                   s[IVNames::MLAI], m_ALA);
+  s[IVNames::CrownCover] = CorrelateValue(Rng(m_CrownCover, m_RNG), 
+                                          s[IVNames::MLAI], m_CrownCover);
+  s[IVNames::HsD] = CorrelateValue(Rng(m_HsD, m_RNG), 
+                                   s[IVNames::MLAI], m_HsD);
+  s[IVNames::N] = CorrelateValue(Rng(m_N, m_RNG), 
+                                 s[IVNames::MLAI], m_N);
+  s[IVNames::Cab] = CorrelateValue(Rng(m_Cab, m_RNG), 
+                                   s[IVNames::MLAI], m_Cab);
+  s[IVNames::Car] = s[IVNames::Cab]*0.25;
+  s[IVNames::Cdm] = CorrelateValue(Rng(m_Cdm, m_RNG), 
+                                   s[IVNames::MLAI], m_Cdm);
+  s[IVNames::CwRel] = CorrelateValue(Rng(m_CwRel, m_RNG), 
+                                     s[IVNames::MLAI], m_CwRel);
+  s[IVNames::Cbp] = CorrelateValue(Rng(m_Cbp, m_RNG), 
+                                   s[IVNames::MLAI], m_Cbp);
+  s[IVNames::Bs] = CorrelateValue(Rng(m_Bs,m_RNG), 
+                                        s[IVNames::MLAI], m_Bs);
 
-    return s;
-  }
+  return s;
+}
 
-  void WriteSample(SampleType s)
-  {
-    auto si = s.begin();
-    while( si != s.end())
-      {
-      m_SampleFile << std::setw(12) << std::left << (*si).second ;
-      ++si;
-      }
-    m_SampleFile << std::endl;
-  }
+void BVInputVariableGeneration::WriteSample(otb::BV::SampleType s)
+{
+  auto si = s.begin();
+  while( si != s.end())
+    {
+    m_SampleFile << std::setw(12) << std::left << (*si).second ;
+    ++si;
+    }
+  m_SampleFile << std::endl;
+}
 
-  void DoExecute()
-  {
-    /*
+void BVInputVariableGeneration::DoExecute()
+{
+  /*
      Car --> not used by bvnet; Féret's dissertation uses Cxc to denote Car
      and gives a mean of 8.58 and a stdev of 3.95 and fig 2.2, p.47 gives the
      min at 0 and the max at 25
@@ -245,10 +207,10 @@ private:
       {
       if( GetParameterString("distlai") == "normal" )
         {
-        m_MLAI.dist = DistType::GAUSSIAN;
+        m_MLAI.dist = otb::BV::DistType::GAUSSIAN;
         }
       }
-    if( m_MLAI.dist == DistType::GAUSSIAN)
+    if( m_MLAI.dist == otb::BV::DistType::GAUSSIAN)
       {
       otbAppLogINFO("LAI distribution is normal\n");
       }
@@ -301,106 +263,5 @@ private:
                   << GetParameterString("out") << std::endl);
   }
 
-
-  VarParams m_MLAI = { 0.0, 15.0, 2.0, 2.0, 0, 0, false, 6, DistType::LOGNORMAL};
-                                                                                            
-  VarParams m_ALA ={  30.0,                      
-                      80.0,                      
-                      60.0,                      
-                      20.0,                      
-                      55,
-                      65,
-                      true,
-                      3,                      
-                      DistType::GAUSSIAN};
-                                                                                            
-  VarParams m_CrownCover ={    0.95,                
-                               1.0,                
-                               0.8,                
-                               0.4,                
-                               0.95,
-                               1.0,
-                               false,
-                               1,                 
-                               DistType::UNIFORM};
-                                                                                            
-  VarParams m_HsD ={     0.1,                       
-                         0.5,                       
-                         0.2,                       
-                         0.5,                       
-                         0.1,
-                         0.5,
-                         true,
-                         1,                       
-                         DistType::GAUSSIAN};
-                                                                                            
-  VarParams m_N ={     1.20,                        
-                       2.20,                        
-                       1.50,                        
-                       0.30,        
-                       1.30,
-                       1.80,
-                       false,                
-                       3,                          
-                       DistType::GAUSSIAN};
-                                                                                            
-  VarParams m_Cab ={     20.0,                      
-                         90.0,                      
-                         45.0,                      
-                         30.0,  
-                         45,
-                         90,
-                         true,                    
-                         4,                        
-                         DistType::GAUSSIAN};
-                                                                                            
-  VarParams m_Car ={     0.0,                       
-                         25.0,                      
-                         8.58,                      
-                         3.95,                      
-                         0, 0, false, 1,
-                         DistType::GAUSSIAN};
-  VarParams m_Cdm ={      0.0030,                    
-                          0.0110,                    
-                          0.0050,                    
-                          0.0050,                    
-                          0.0050,
-                          0.0110,
-                          true,                    
-                          4,                        
-                          DistType::GAUSSIAN};
-
-  VarParams m_CwRel ={                                                                                                
-    0.60,                    
-    0.85,                    
-    0.75,                    
-    0.08,       
-    0.70,
-    0.80,
-    true,                                 
-    4,                      
-    DistType::UNIFORM};
-                                                                                          
-
-  VarParams m_Cbp ={         0.00,                      
-                             2.00,                      
-                             0.00,                      
-                             0.30,         
-                             0.00,
-                             0.20,
-                             true,                                 
-                             3,                        
-                             DistType::GAUSSIAN  };
-  VarParams m_Bs ={ 0.0, 1.00, 0.5, 2.00, 0.50, 1.20, false, 4, DistType::GAUSSIAN};
-
-  // the random number generator
-    std::mt19937 m_RNG;
-
-  // the output file
-  std::ofstream m_SampleFile;
-};
-
-}
-}
-
-OTB_APPLICATION_EXPORT(otb::Wrapper::BVInputVariableGeneration)
+}//namespace Wrapper
+}//namespace otb
---
+++ src/applications/otbBVInputVariableGeneration.h
@@ -0,0 +1,77 @@
+/*=========================================================================
+  Program:   otb-bv
+  Language:  C++
+
+  Copyright (c) CESBIO. All rights reserved.
+
+  See otb-bv-copyright.txt for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "otbWrapperApplication.h"
+#include "otbWrapperApplicationFactory.h"
+#include "otbWrapperChoiceParameter.h"
+#include "otbBVTypes.h"
+
+namespace otb
+{
+
+namespace Wrapper
+{
+
+namespace BV
+{
+//Generates a random number of the appropriate distribution and respecting the bounds
+template<typename RNGType>
+double Rng(otb::BV::VarParams vpars, RNGType rngen);
+double CorrelateValue(double v, double lai, otb::BV::VarParams vpars);
+}//namespace BV
+
+class BVInputVariableGeneration : public Application
+{
+public:
+/** Standard class typedefs. */
+  typedef BVInputVariableGeneration     Self;
+  typedef Application                   Superclass;
+
+
+  /** Standard macro */
+  itkNewMacro(Self);
+  itkTypeMacro(BVInputVariableGeneration, otb::Application);
+
+
+private:
+  void DoInit();
+  virtual ~BVInputVariableGeneration(){}
+  void DoUpdateParameters() {}
+  ///Builds the map with the values of the sample
+  otb::BV::SampleType DrawSample();
+  void WriteSample(otb::BV::SampleType s);
+  void DoExecute();
+
+  otb::BV::VarParams m_MLAI = {0.0, 15.0, 2.0, 2.0, 0, 0, false, 6, otb::BV::DistType::LOGNORMAL};
+  otb::BV::VarParams m_ALA ={30.0, 80.0, 60.0, 20.0, 55, 65, true, 3, otb::BV::DistType::GAUSSIAN};
+  otb::BV::VarParams m_CrownCover ={0.95, 1.0, 0.8, 0.4, 0.95, 1.0, false, 1, otb::BV::DistType::UNIFORM};
+  otb::BV::VarParams m_HsD ={0.1, 0.5, 0.2, 0.5, 0.1, 0.5, true, 1, otb::BV::DistType::GAUSSIAN};
+  otb::BV::VarParams m_N ={1.20, 2.20, 1.50, 0.30, 1.30, 1.80, false, 3, otb::BV::DistType::GAUSSIAN};
+  otb::BV::VarParams m_Cab ={20.0, 90.0, 45.0, 30.0, 45, 90, true, 4, otb::BV::DistType::GAUSSIAN};
+  otb::BV::VarParams m_Car ={0.0, 25.0, 8.58, 3.95, 0, 0, false, 1, otb::BV::DistType::GAUSSIAN};
+  otb::BV::VarParams m_Cdm ={0.0030, 0.0110, 0.0050, 0.0050, 0.0050, 0.0110, true, 4, otb::BV::DistType::GAUSSIAN};
+  otb::BV::VarParams m_CwRel ={0.60, 0.85, 0.75, 0.08, 0.70, 0.80, true, 4, otb::BV::DistType::UNIFORM};
+  otb::BV::VarParams m_Cbp ={0.00, 2.00, 0.00, 0.30, 0.00, 0.20, true, 3, otb::BV::DistType::GAUSSIAN  };
+  otb::BV::VarParams m_Bs ={ 0.0, 1.00, 0.5, 2.00, 0.50, 1.20, false, 4, otb::BV::DistType::GAUSSIAN};
+
+  // the random number generator
+  std::mt19937 m_RNG;
+  // the output file
+  std::ofstream m_SampleFile;
+};
+
+}
+}
+
+OTB_APPLICATION_EXPORT(otb::Wrapper::BVInputVariableGeneration)
