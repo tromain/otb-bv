b25b7839b0f9437e455f1d7b9f8e348171279c63
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2016-05-30 22:36:40 +0200

    ENH: script for the generation of config files for parameter sweep

---
+++ src/scripts/parameter-sweep.py
@@ -0,0 +1,45 @@
+
+rsr_dir = "/home/inglada/Dev/otb-bv/data/"
+nbSamples_train = 500
+nbSamples_test = 20
+noise_std = 0.001
+nthreads = 32
+config_path = "/tmp/"
+
+for useVI in ["yes","no"]:
+    for distlai in ["normal", "lognormal"]:
+        for stdlai in range(10,1000,10):
+            for modlai in range(0,1000,25):
+                for maxlai in range(300,1500,100):
+                    cfgname = useVI+"_"+distlai+"_"+str(maxlai)+"_"+str(modlai)+"_"+str(stdlai)+".cfg"
+                    with open(config_path+cfgname, 'w') as cfgf:
+                        cfgf.write("""paths:
+                        {
+                        wdpath : \"/tmp\"
+                        varName : \"MLAI\"
+                        rsr_dir : \""""+rsr_dir+"""\"
+                        input_var_file : \"input-vars\"
+                        input_var_file_test : \"input-vars-test\"
+                        }
+
+                        simulation:
+                        {
+                        simulate : \"yes\"
+                        nbSamples_train : """+str(nbSamples_train)+"""
+                        nbSamples_test : """+str(nbSamples_test)+"""
+                        noise_std : """+str(noise_std)+"""
+                        minlai : 0.0
+                        maxlai : """+str(maxlai/100.0)+"""
+                        modlai : """+str(modlai/100.0)+"""
+                        stdlai : """+str(stdlai/100.0)+"""
+                        distlai : \""""+distlai+"""\"
+                        useVI : \""""+useVI+"""\"
+                        nthreads : """+str(nthreads)+"""
+                        }
+
+                        inversion:
+                        {
+                        bestof : 1
+                        regressor : \"nn\" # nn svr rfr mlr
+                        }
+                        """ )
