60c8053486c425b525091f5b803ee781c33f0a72
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2016-12-07 10:21:38 +0100

    COMP: require c++11

--- otb-module.cmake
+++ otb-module.cmake
@@ -1,5 +1,7 @@
 set(DOCUMENTATION "Biophysical variable estimation from optical imagery.")
 
+otb_module_requires_cxx11()
+
 # define the dependencies of the include module and the tests
 otb_module(OTBBioVars
   DEPENDS
