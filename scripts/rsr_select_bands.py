2b3fafd6d2a7aa70cd1be787a28a20cb6da5c9a5
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2017-02-16 10:30:39 +0100

    ADD: RSR band select

---
+++ scripts/rsr_select_bands.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# =========================================================================
+#   Program:   otb-bv
+#   Language:  python
+#
+#   Copyright (c) CESBIO. All rights reserved.
+#
+#   See otb-bv-copyright.txt for details.
+#
+#   This software is distributed WITHOUT ANY WARRANTY; without even
+#   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the above copyright notices for more information.
+#
+# =========================================================================
+
+import string
+import sys
+
+def rsr_select_bands(input_rsr_file, output_rsr_file, band_list):
+    print "Selecting bands "+string.join(sorted(band_list),', ')
+    with open(input_rsr_file, 'r') as irsr:
+        with open(output_rsr_file, 'w') as orsr:
+            for l in irsr.readlines():
+                fields = string.split(l)
+                ol = fields[0]+" "+fields[1]
+                for b in sorted(band_list):
+                    ol += " "+fields[int(b)+1]
+                orsr.write(ol+"\n")
+    
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print """
+        Script to select a subset of the spectral band of an RSR file and produce a new SRS file.
+        """
+        print "Usage: "+sys.argv[0]+" input_file output_file band_list [starting with 1, white space separated]"
+    else:
+        rsr_select_bands(sys.argv[1],sys.argv[2],sys.argv[3:])
