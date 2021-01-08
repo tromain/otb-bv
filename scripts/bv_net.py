83b4510a26fd621978dee626c927c99603f82bae
Thibaut ROMAIN <thibaut.romain@csgroup.eu>
2021-01-08 17:18:33 +0100

    update bv_net to work properly with current otb and python3

--- scripts/bv_net.py
+++ scripts/bv_net.py
@@ -16,7 +16,6 @@
 
 import sys
 import os
-import string
 from config import Config
 import otbApplication as otb
 
@@ -25,18 +24,39 @@ bvindex = {"MLAI": 0, "ALA": 1, "CrownCover": 2, "HsD": 3, "N": 4, "Cab": 5, "Ca
 
 bv_val_names = {"MLAI": ['gai', 'lai-bvnet'], "FAPAR": ['fapar', 'fapar-bvnet'], "FCOVER": ['fcover', 'fcover-bvnet']}
 
+
 def parseConfigFile(cfg):
-    distFileName = cfg.bvDistribution.fileName
-    nSamples = cfg.bvDistribution.samples
+    distFileName = cfg['bvDistribution.fileName']
+    nSamples = cfg['bvDistribution.samples']
     simuPars = {}
-    simuPars['rsrFile'] = cfg.simulation.rsrFile
-    simuPars['outputFile'] = cfg.simulation.outputFile
-    simuPars['solarZenithAngle'] = cfg.simulation.solarZenithAngle
-    simuPars['sensorZenithAngle'] = cfg.simulation.sensorZenithAngle
-    simuPars['solarSensorAzimuth'] = cfg.simulation.solarSensorAzimuth
-    trainingFile = cfg.training.fileName
-    modelFile = cfg.learning.outputFileName
-    return (distFileName, nSamples, simuPars, trainingFile, modelFile)
+    simuPars['rsrFile'] = cfg['simulation.rsrFile']
+    simuPars['outputFile'] = cfg['simulation.outputFile']
+    simuPars['solarZenithAngle'] = cfg['simulation.solarZenithAngle']
+    simuPars['sensorZenithAngle'] = cfg['simulation.sensorZenithAngle']
+    simuPars['solarSensorAzimuth'] = cfg['simulation.solarSensorAzimuth']
+
+    #### AJOUT pour exécution bv_net------- debut
+    simuPars['minlai'] = cfg['simulation.minlai']
+    simuPars['maxlai'] = cfg['simulation.maxlai']
+    simuPars['modlai'] = cfg['simulation.modlai']
+    simuPars['stdlai'] = cfg['simulation.stdlai']
+    simuPars['distlai'] = cfg['simulation.distlai']
+    simuPars['minbs'] = cfg['simulation.minbs']
+    simuPars['maxbs'] = cfg['simulation.maxbs']
+    simuPars['modbs'] = cfg['simulation.modbs']
+    simuPars['stdbs'] = cfg['simulation.stdbs']
+    simuPars['soilindexmax'] = cfg['simulation.soilindexmax']
+    simuPars['noisestd'] = cfg['simulation.noisestd']
+    simuPars['threads'] = cfg['simulation.threads']
+    simuPars['useSoilDB'] = cfg['simulation.useSoilDB']
+    simuPars['soilfile'] = cfg['simulation.soilfile']
+    simuPars['soilwlfactor'] = cfg['simulation.soilwlfactor']
+    #### AJOUT ------- fin
+
+    trainingFile = cfg['training.fileName']
+    modelFile = cfg['learning.outputFileName']
+    normalizationFile = cfg['learning.normalizationFile']
+    return (distFileName, nSamples, simuPars, trainingFile, modelFile,normalizationFile)
 
 def addVI(reflectances_file, red_index, nir_index):
     rff = open(reflectances_file)
@@ -44,9 +64,9 @@ def addVI(reflectances_file, red_index, nir_index):
     rff.close()
     with open(reflectances_file, 'w') as rf:
         for l in allfields:
-            rfls = string.split(l)
+            rfls = str.split(l)
             if len(rfls)>red_index:
-                outline = string.join(string.split(l))
+                outline = str.join(str.split(l))
                 red = float(rfls[red_index-1])
                 pir = float(rfls[nir_index-1])
                 epsilon = 0.001
@@ -67,7 +87,7 @@ def generateInputBVDistribution(bvFile, nSamples, simuPars):
     app.SetParameterString("out", bvFile)
     app.ExecuteAndWriteOutput()
 
-def generateTrainingData(bvFile, simuPars, trainingFile, bvidx, simulate=True, add_angles=False, red_index=0, nir_index=0, nthreads=2):
+def generateTrainingData(bvFile, simuPars, trainingFile, bvidx, add_angles=False, red_index=0, nir_index=0, nthreads=2):
     """
     Generate a training file using the file of biophysical vars (bvFile) and the simulation parameters dictionary (simuPars).
     Write the result to trainingFile. The first column will be the biovar to learn and the following columns will be 
@@ -75,17 +95,16 @@ def generateTrainingData(bvFile, simuPars, trainingFile, bvidx, simulate=True, a
     nir_index are set, the ndvi and the rvi are also used as features. red_index=3 means that the red reflectance is the 3rd column
     (starting at 1) in the reflectances file.
     """
-    if simulate :
-        app = otb.Registry.CreateApplication("ProSailSimulator")
-        app.SetParameterString("bvfile", bvFile)
-        app.SetParameterString("rsrfile", simuPars['rsrFile'])
-        app.SetParameterString("out", simuPars['outputFile'])
-        app.SetParameterFloat("solarzenith", simuPars['solarZenithAngle'])
-        app.SetParameterFloat("sensorzenith", simuPars['sensorZenithAngle'])
-        app.SetParameterFloat("azimuth", simuPars['solarSensorAzimuth'])
-        app.SetParameterStringList("noisestd", [str(simuPars['noisestd'])])
-        app.SetParameterInt("threads", nthreads)
-        app.ExecuteAndWriteOutput()
+    app = otb.Registry.CreateApplication("ProSailSimulator")
+    app.SetParameterString("bvfile", bvFile)
+    app.SetParameterString("rsrfile", simuPars['rsrFile'])
+    app.SetParameterString("out", simuPars['outputFile'])
+    app.SetParameterFloat("solarzenith", simuPars['solarZenithAngle'])
+    app.SetParameterFloat("sensorzenith", simuPars['sensorZenithAngle'])
+    app.SetParameterFloat("azimuth", simuPars['solarSensorAzimuth'])
+    app.SetParameterStringList("noisestd", [str(simuPars['noisestd'])])
+    app.SetParameterInt("threads", nthreads)
+    app.ExecuteAndWriteOutput()
     #combine the bv samples, the angles and the simulated reflectances for variable inversion and produce the training file
     with open(trainingFile, 'w') as tf:
         with open(bvFile, 'r') as bvf:
@@ -95,21 +114,21 @@ def generateTrainingData(bvFile, simuPars, trainingFile, bvidx, simulate=True, a
                 for (refline, bvline) in zip(rf.readlines(), bvf.readlines()):
                     outline = ""
                     if bvidx == bvindex["FCOVER"] :
-                        outline = string.split(refline)[-1]
+                        outline = str.split(refline)[-1]
                     elif bvidx == bvindex["FAPAR"] : 
-                        outline = string.split(refline)[-2]
+                        outline = str.split(refline)[-2]
                     else:
-                        outline = string.split(bvline)[bvidx]
-                    outline = outline+" "+string.join(string.split(refline[:-1])[:-2], ' ')
+                        outline = str.split(bvline)[bvidx]
+                    outline = outline+" "+refline[:-2]
                     outline.rstrip()
                     if add_angles:
-                        angles = `simuPars['solarZenithAngle']`+" "+`simuPars['sensorZenithAngle']`+" "+`simuPars['solarSensorAzimuth']`
+                        angles = simuPars['solarZenithAngle']+" "+simuPars['sensorZenithAngle']+" "+simuPars['solarSensorAzimuth']
                         outline += " "+angles
                     outline += "\n"
                     tf.write(outline)
     if red_index!=0 and nir_index!=0:
         # we need to add 1 to the indices since the file already contains the variable in the first column
-        print "Adding VIs for training data"
+        print ("Adding VIs for training data")
         addVI(trainingFile, red_index+1, nir_index+1)
                 
                 
@@ -131,11 +150,11 @@ def invertBV(reflectanceFile, modelFile, normalizationFile, outputFile, removeFa
         rff.close()
         with open(reflectanceFile, 'w') as rf:
             for l in allfields:
-                outline = string.join(string.split(l)[:-2])+"\n"
+                outline = str.join(l[:-2], ' ')+"\n"
                 rf.write(outline)
 
     if red_index!=0 and nir_index!=0:
-        print "Adding VIs for inversion"
+        print ("Adding VIs for inversion")
         addVI(reflectanceFile, red_index, nir_index)
 
                 
@@ -151,12 +170,12 @@ if __name__ == '__main__':
         sys.exit('Usage: %s <configurationFilePath>.cfg' % sys.argv[0])
     if not os.path.exists(sys.argv[1]):
         sys.exit('ERROR: Configuration File path %s was not found!'% sys.argv[1] )
-    f = file ( sys.argv[1] )
+    f = open( sys.argv[1],'r' )
     cfg=Config(f)
 
-    (bvDistributionFileName, numberOfSamples, simulationParameters, trainingDataFileName, outputModelFileName) = parseConfigFile(cfg)
-    generateInputBVDistribution(bvDistributionFileName, numberOfSamples)
-    generateTrainingData(bvDistributionFileName, simulationParameters, trainingDataFileName, bvindex[cfg.training.invertBV])
-    learnBVModel(trainingDataFileName, outputModelFileName)
+    (bvDistributionFileName, numberOfSamples, simulationParameters, trainingDataFileName, outputModelFileName,normFile) = parseConfigFile(cfg)
+    generateInputBVDistribution(bvDistributionFileName, numberOfSamples, simulationParameters)
+    generateTrainingData(bvDistributionFileName, simulationParameters, trainingDataFileName, bvindex[cfg['training.invertBV']])
+    learnBVModel(trainingDataFileName, outputModelFileName,"rfr",normFile)
 
 
