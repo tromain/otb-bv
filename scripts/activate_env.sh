2111a7316acea476f46b05cb034a0a4d8f20ce21
Thibaut ROMAIN <thibaut.romain@csgroup.eu>
2021-01-11 17:53:08 +0100

    add a script to activate OTB environment variables for the python scripts

---
+++ scripts/activate_env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "--- Start ---"
+
+export OTB_RESULT_DIR=/home/thibaut/Documents/output/bv-net
+export OTB_DATA_DIR=/home/thibaut/OTB/otb/Modules/Remote/OTBBioVars/data
+export PYTHONPATH=/home/thibaut/OTB/build/lib/otb/python
+export OTB_APPLICATION_PATH=/home/thibaut/OTB/build/lib/otb/applications
