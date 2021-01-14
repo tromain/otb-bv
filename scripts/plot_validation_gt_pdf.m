8d907a240af7ec6587aa5bd4b101c738c7a853bd
Thibaut ROMAIN <thibaut.romain@csgroup.eu>
2021-01-14 10:25:32 +0100

    add an Octave script to print the validation result to a pdf file

---
+++ scripts/plot_validation_gt_pdf.m
@@ -0,0 +1,23 @@
+satnames = {"formosat2","s2","landsat8","spot4","spot5"};
+cd "/tmp/MLAI/"
+allDirs = dir("/tmp/MLAI");
+hf = figure();
+for i = 3:size(allDirs)
+  satfound = 0;
+  currDir = allDirs(i).name;
+  currPath = strcat("/tmp/MLAI/",currDir);
+  for currSat = satnames
+    if(findstr(currDir,currSat{1}) == 1)
+      satfound = 1;
+    endif
+  endfor
+  if(satfound == 1)
+    cd (currPath)
+    load validation_gt_nn
+    clf
+    plot (validation_gt_nn)
+    legend ("Ground Truth","BVNET","NN")
+    title (currDir)
+    print (hf, "results.pdf", "-dpdflatex")
+  endif
+endfor
