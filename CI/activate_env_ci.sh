338f94c52a2c35d1aaaa37bf10174a42c24a7f68
Thibaut ROMAIN <thibaut.romain@csgroup.eu>
2021-01-22 09:52:28 +0100

    CI: add correct path of otb-bv standalone module for python test scripts

--- CI/activate_env_ci.sh
+++ CI/activate_env_ci.sh
@@ -5,4 +5,4 @@ echo "--- Start ---"
 export OTB_RESULT_DIR=/opt/otb-bv/scripts/output
 export OTB_DATA_DIR=/opt/otb-bv/data
 export PYTHONPATH=/opt/otb/lib/otb/python
-export OTB_APPLICATION_PATH=/opt/otb/lib/otb/applications
+export OTB_APPLICATION_PATH=/opt/otb-bv/install/lib:/opt/otb/lib/otb/applications
